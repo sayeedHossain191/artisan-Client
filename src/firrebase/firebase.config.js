4ecdf0daf5cbdfde31e9c465a905c968a6922580
sayeedHossain191 <sayeed.hossain.sagor@gmail.com>
2024-04-26 10:05:40 +0600

    first commit

---
+++ src/firrebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDLuLVx49Bl_XjxKAqgHKwwGdpM4PO8AVg",
+    authDomain: "b9a10-art-and-craft-client.firebaseapp.com",
+    projectId: "b9a10-art-and-craft-client",
+    storageBucket: "b9a10-art-and-craft-client.appspot.com",
+    messagingSenderId: "738093746288",
+    appId: "1:738093746288:web:c0702fd759368226298cb5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
